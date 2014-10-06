a609caece49a408131a92663641cbb0621a97f5f
gavincook <swbyzx@gmail.com>
2014-10-06 13:42:30 +0800

    #83 refactor permission module for not load items into domain container when do query

--- src/main/java/org/moon/rbac/domain/eventhandler/PermissionEventHandler.java
+++ src/main/java/org/moon/rbac/domain/eventhandler/PermissionEventHandler.java
@@ -3,30 +3,39 @@ package org.moon.rbac.domain.eventhandler;
 import com.reeham.component.ddd.annotation.OnEvent;
 import com.reeham.component.ddd.model.ModelContainer;
 import org.moon.base.domain.eventhandler.BaseEventHandler;
+import org.moon.core.orm.mybatis.DataConverter;
+import org.moon.rbac.domain.Menu;
 import org.moon.rbac.domain.Permission;
 import org.moon.rbac.repository.PermissionRepository;
 import org.moon.rbac.service.PermissionService;
+import org.moon.utils.Domains;
+import org.moon.utils.Dtos;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Gavin
  * @date Jul 9, 2014
  */
 @Component
-public class PermissionEventHandler extends BaseEventHandler<Permission, PermissionService>{
+public class PermissionEventHandler extends BaseEventHandler<Permission, PermissionService> {
 
-	@Resource
-	private PermissionRepository permissionRepository;
-	
-	@Resource
-	private ModelContainer modelContainer;
-	
-	@OnEvent("permission/getPermissionForRole")
-	public List<Permission> getPermissionForRole(Long rid){
-		return modelContainer.identifiersToModels((List)permissionRepository.getPermissionsByRole(rid),Permission.class,this.service);
-	}
-	
+    private DataConverter<Map> dataConverter = new DataConverter<Map>() {
+        @Override
+        public Object convert(Map map) {
+            return Domains.convertMapToDomain(map, Permission.class);
+        }
+    };
+
+    @Resource
+    private PermissionRepository permissionRepository;
+
+    @OnEvent("permission/getPermissionForRole")
+    public List<Permission> getPermissionForRole(Long rid) {
+        return Dtos.convert(permissionRepository.getPermissionsByRole(rid),dataConverter);
+    }
+
 }
--- src/main/java/org/moon/rbac/repository/PermissionRepository.java
+++ src/main/java/org/moon/rbac/repository/PermissionRepository.java
@@ -18,16 +18,12 @@ import java.util.Map;
 @Repository
 public interface PermissionRepository extends BaseRepository<Permission>{
 
-	
-	
 	public void batchSave(@Param("permissions")List<Permission> permissions);
 	
 	public void assignAddPermission(@Param("pids")Long[] pids,@Param("rids")Long[] rids);
-	
+
 	public void assignDeletePermission(@Param("pids")Long[]  pids,@Param("rids")Long[] rids);
-	
-	public List<Long> getPermissionsByRole(@Param("rid")Long rid);
-	
-	public Integer getPermissionsByRole_count(Map<String,Object> paramsMap);
-	
+
+	public List<Map> getPermissionsByRole(@Param("rid")Long rid);
+
 }
